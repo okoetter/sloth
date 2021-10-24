aab74ebc1344d7794e0c3367ebbe7f2892110a8e
Oliver Kötter <oliver@koetter.cc>
2021-10-24 14:23:16 +0200

    Implement syntax checking for Load() and Save()

--- Slofile.cs
+++ Slofile.cs
@@ -25,33 +25,52 @@ namespace sloth
   public static class SloCommands
   {
     /// <summary>
-    /// checks a command and its arguments for correct syntax
+    /// checks a script for correct syntax
     /// </summary>
-    /// <param name="command">command</param>
-    /// <param name="args">arguments</param>
-    static List<string> CheckCommand(string command, string[] args) {
+    /// <param name="script">command</param>
+    /// <returns>list of error messages</returns>
+    static List<string> CheckSyntax(SloScript script) {
       List<string> errors = new List<string>();
 
-      // build argument types
-      List<SloTypes> types = new List<SloTypes>();
-      foreach(string arg in args)
+      foreach (SloCommand command in script)
       {
-        int quoteCount = arg.Count(c => c == '"');
-        int squareBracketOpenCount = arg.Count(c => c == '[');
-        int squareBracketCloseCount = arg.Count(c => c == ']');
+        // build argument types
+        List<SloTypes> types = new List<SloTypes>();
+        foreach(string arg in command.Arguments)
+        {
+          int quoteCount = arg.Count(c => c == '"');
+          int squareBracketOpenCount = arg.Count(c => c == '[');
+          int squareBracketCloseCount = arg.Count(c => c == ']');
 
-        if(Regex.IsMatch(arg, @"^\s*""") && Regex.IsMatch(arg, @"\s""*$") && quoteCount == 2) types.Add(SloTypes.String);
-        else if(Regex.IsMatch(arg, @"^\s*\[") && Regex.IsMatch(arg, @"\]\s*$") && squareBracketOpenCount == 1 && squareBracketCloseCount == 1) types.Add(SloTypes.Array);
-        else types.Add(SloTypes.Unknown);
-      }
+          if(Regex.IsMatch(arg, @"^\s*""") && Regex.IsMatch(arg, @"\s""*$") && quoteCount == 2) types.Add(SloTypes.String);
+          else if(Regex.IsMatch(arg, @"^\s*\[") && Regex.IsMatch(arg, @"\]\s*$") && squareBracketOpenCount == 1 && squareBracketCloseCount == 1) types.Add(SloTypes.Array);
+          else types.Add(SloTypes.Unknown);
+        }
 
-      switch(command.ToLower()) {
-        case "load":
-          if(args.Length != 2) errors.Add("Two arguments expected for Load() in line ");
-          if(types[0] != SloTypes.String) errors.Add("First argument of Load() must be of type string in line ");
-          if(types[1] != SloTypes.String) errors.Add("Second argument of Load() must be of type string in line ");
-          if(types[0] == SloTypes.String && !File.Exists(args[0])) errors.Add("File not found in line ");
-          break;
+        // check commands          
+        switch(command.Command.ToLower()) {
+          // load(filename: string, type: "xlsx" | "\t" | ";" | ",")
+          case "load":
+            if(command.Arguments.Length != 2) errors.Add($"Two arguments expected for Load() in line {command.LineNo}");
+            if(types[0] != SloTypes.String) errors.Add($"First argument of Load() must be of type string in line {command.LineNo}");
+            if(types[1] != SloTypes.String) errors.Add($"Second argument of Load() must be of type string in line {command.LineNo}");
+            if(types[0] == SloTypes.String && !File.Exists(command.Arguments[0])) errors.Add($"File not found in line {command.LineNo}");
+            if(!new[] { "xlsx", "\t", ";", "," }.Contains(command.Arguments[1].ToLower())) errors.Add($"Invalid file type '{command.Arguments[1]}' in line {command.LineNo}");
+            break;
+
+          // save(filename: string, type: "xlsx" | "\t" | ";" | ",")
+          case "save":
+            if(command.Arguments.Length != 2) errors.Add($"Two arguments expected for Save() in line {command.LineNo}");
+            if(types[0] != SloTypes.String) errors.Add($"First argument of Save() must be of type string in line {command.LineNo}");
+            if(types[1] != SloTypes.String) errors.Add($"Second argument of Save() must be of type string in line {command.LineNo}");
+            if(types[0] == SloTypes.String && !File.Exists(command.Arguments[0])) errors.Add($"File not found in line {command.LineNo}");
+            if(!new[] { "xlsx", "\t", ";", "," }.Contains(command.Arguments[1].ToLower())) errors.Add($"Invalid file type '{command.Arguments[1]}' in line {command.LineNo}");
+            break;
+
+          default:
+            errors.Add($"Unknown command in line {command.LineNo}");
+            break;
+        }
       }
 
       return errors;
